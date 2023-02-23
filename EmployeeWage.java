e2cb5d6f7e44353f47897858b97b874e836748c2
ajayjagtap-1404 <ajayjagtap1404@gmail.com>
2023-02-23 12:19:39 +0530

    Start : EmpWage Program

---
+++ EmployeeWage.java
@@ -0,0 +1,7 @@
+package com.assigenement.day_3.emp_wage;
+
+public class EmployeeWage {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Program :");
+    }
+}
