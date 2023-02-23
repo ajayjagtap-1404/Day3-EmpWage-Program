bcd86640936ca438f556afabc0b223d0ac3fe4a5
ajayjagtap-1404 <ajayjagtap1404@gmail.com>
2023-02-23 12:56:54 +0530

    Adding part time empWage program

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -5,17 +5,27 @@ public class EmployeeWage {
         System.out.println("Welcome to Employee Wage Program :");
 
         int IS_FULL_TIME = 1;
+        int IS_PART_TIME = 2;
         int EMP_RATE_PER_HR = 20;
 
         int empHrs = 0;
         int empWage = 0;
 
-        double empCheck = Math.floor(Math.random() * 10 ) % 2;
+        double empCheck = Math.floor(Math.random() * 10 ) % 3;
 
         if (empCheck == IS_FULL_TIME) {
             empHrs = 8;
+            System.out.println("Employee is Present ");
+        } else if (empCheck == IS_PART_TIME){
+            empHrs = 4;
+            System.out.println("Employee is present half time ");
+
+
         } else {
             empHrs = 0;
+            System.out.println("Employee is Absent");
+
+
         }
         empWage = empHrs * EMP_RATE_PER_HR;
         System.out.println("Emp Wage: " + empWage);
